ae8a29cd18b726e406f9d4b2ce3019e25d606270
naveen-672 <190330175cse@gmail.com>
2026-01-26 12:15:03 +0530

    26 jan commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Search from "./components/Search";
+import FoodList from "./components/FoodList";
+import Nav from "./components/Nav";
+import "./App.css";
+import Container from "./components/Container";
+import InnerContainer from "./components/InnerContainer";
+
+function App() {
+  const [foodData, setFoodData] = useState([]);
+
+  return (
+    <div className="App">
+      <Nav />
+      <Search foodData={foodData} setFoodData={setFoodData} />
+      <Container>
+        <InnerContainer>
+          <FoodList foodData={foodData} />
+        </InnerContainer>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Search.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import styles from "./search.module.css";
+
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "3240940e5bd14082b94582826b081683";
+
+export default function Search({ foodData, setFoodData }) {
+  const [query, setQuery] = useState("pizza");
+
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      setFoodData(data.results);
+      console.log(data.results);
+    }
+    fetchFood();
+  }, [query]);
+
+  return (
+    <div className={styles.searchContainer}>
+      <input
+        className={styles.input}
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </div>
+  );
+}
