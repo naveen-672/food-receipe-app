cc8b31eccca18bf380d00cbbe82f279db98ffd6c
naveen-672 <190330175cse@gmail.com>
2026-02-21 22:45:22 +0530

    updated code

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -3,12 +3,12 @@ import Item from "./Item";
 export default function ItemList({ food, isLoading }) {
   return (
     <div>
+      <h2>Ingredients</h2>
       {isLoading ? (
         <p>Loading...</p>
       ) : (
         food.extendedIngredients.map((item) => <Item item={item} />)
       )}
-      <h2>Ingredients</h2>
     </div>
   );
 }
